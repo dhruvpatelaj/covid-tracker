8054b8deeb7110d14ab32688df987643eb37e090
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-13 16:58:09 -0600

    api calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.css";
 
-function App() {
-    return (
-        <div className="App">
-            <h1>App.js</h1>
-        </div>
-    );
+import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+    async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
+    render() {
+        return (
+            <div className={styles.container}>
+                <h1>App.js</h1>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
 }
 
 export default App;
