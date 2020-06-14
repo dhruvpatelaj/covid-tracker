43b08bbd9749fabcc80fa6d5eeab4cc365a79c45
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-13 19:33:11 -0600

    add api values to each card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import CountUp from "react-countup";
 import styles from "./Cards.module.css";
+import cx from "classnames";
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     //~ [num infected] [num recovered] [num death]
     //~ materialUI is basically bootstrap for REACT
@@ -12,7 +13,14 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.infected)}
+                >
+                    {/*//~ Infected */}
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Infected
@@ -25,33 +33,67 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                                 separator=","
                             />
                         </Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography color="textSecondary">
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
                         <Typography variant="body2">
                             Number of Active Cases of COVID-19
                         </Typography>
                     </CardContent>
                 </Grid>
 
-                <Grid item component={Card}>
+                {/*//~ Recovered */}
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.recovered)}
+                >
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Recovered
                         </Typography>
-                        <Typography variant="h5">RealData </Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>{" "}
                         <Typography variant="body2">
                             Number of Recoveries from COVID-19
                         </Typography>
                     </CardContent>
                 </Grid>
 
-                <Grid item component={Card}>
+                {/*//~ Deaths */}
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.deaths)}
+                >
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>
                             Deaths
                         </Typography>
-                        <Typography variant="h5">RealData </Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>{" "}
                         <Typography variant="body2">
                             Number of Deaths caused by COVID-19
                         </Typography>
