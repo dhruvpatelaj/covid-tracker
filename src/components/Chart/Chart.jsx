8ad1be95c71e54e17ae756f8211957f654587300
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-13 21:07:58 -0600

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,7 +5,7 @@ import { fetchDailyData } from "../../api";
 
 import styles from "./Chart.module.css";
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     const [dailyData, setDailyData] = useState([]);
 
     //! async use effect does not exist. effect callbacks are SYNCHRONOUS
@@ -42,7 +42,32 @@ const Chart = () => {
         />
     );
 
-    return <div className={styles.container}> {lineChart} </div>;
+    const barChart = confirmed ? (
+        <Bar
+            data={{
+                labels: ["Infected", "Recovered", "Deaths"],
+                datasets: [
+                    {
+                        label: "People",
+                        backgroundColor: [
+                            "rgba(0, 0, 255, 0.5)",
+                            "rgba(0, 255, 0, 0.5)",
+                            "rgba(255, 0, 0, 0.5)",
+                        ],
+                        data: [confirmed.value, recovered.value, deaths.value],
+                    },
+                ],
+            }}
+            options={{
+                legend: { display: false },
+                title: { display: true, text: `Current state in ${country}` },
+            }}
+        />
+    ) : null;
+
+    return (
+        <div className={styles.container}>{country ? barChart : lineChart}</div>
+    );
 };
 
 export default Chart;
