1ed69110da2283f38cb3dee8d0cb45c945ebf821
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-13 20:14:47 -0600

    some progress

--- src/api/index.js
+++ src/api/index.js
@@ -2,15 +2,46 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-//~ using async with await
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url;
+
+    if (country) {
+        changeableUrl = `${url}/countries/${country}`;
+    }
+
     try {
-        //~ gets the data using axios and specifies those four
         const {
             data: { confirmed, recovered, deaths, lastUpdate },
-        } = await axios.get(url);
+        } = await axios.get(changeableUrl);
 
-        //~ returns those four values
         return { confirmed, recovered, deaths, lastUpdate };
-    } catch (error) {}
+    } catch (error) {
+        return error;
+    }
+};
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        return data.map(({ confirmed, deaths, reportDate: date }) => ({
+            confirmed: confirmed.total,
+            deaths: deaths.total,
+            date,
+        }));
+    } catch (error) {
+        return error;
+    }
+};
+
+export const fetchCountries = async () => {
+    try {
+        const {
+            data: { countries },
+        } = await axios.get(`${url}/countries`);
+
+        return countries.map((country) => country.name);
+    } catch (error) {
+        return error;
+    }
 };
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Line, Bar } from "react-chartjs-2";
+
+import { fetchDailyData } from "../../api";
+
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-    return <h1> Chart </h1>;
+    const [dailyData, setDailyData] = useState({});
+
+    //! async use effect does not exist. effect callbacks are SYNCHRONOUS
+    useEffect(() => {
+        const fetchMyAPI = async () => {
+            const initialDailyData = await fetchDailyData();
+
+            setDailyData(initialDailyData);
+        };
+
+        fetchMyAPI();
+    }, []);
+
+    const lineChart = dailyData[0].length ? (
+        <Line
+            data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [
+                    {
+                        data: dailyData.map((data) => data.confirmed),
+                        label: "Infected",
+                        borderColor: "#3333ff",
+                        fill: true,
+                    },
+                    {
+                        data: dailyData.map((data) => data.deaths),
+                        label: "Deaths",
+                        borderColor: "red",
+                        backgroundColor: "rgba(255, 0, 0, 0.5)",
+                        fill: true,
+                    },
+                ],
+            }}
+        />
+    ) : null;
+
+    return <div> {lineChart} </div>;
 };
 
 export default Chart;
