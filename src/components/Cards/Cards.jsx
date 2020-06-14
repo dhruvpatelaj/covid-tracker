bb522689af84767438682e0989c673c9d9ae04fc
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-13 18:04:00 -0600

    countup animations

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
 import styles from "./Cards.module.css";
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     //~ [num infected] [num recovered] [num death]
     //~ materialUI is basically bootstrap for REACT
-
+    console.log(confirmed);
+    if (!confirmed) {
+        return "Loading...";
+    }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
@@ -14,7 +17,14 @@ const Cards = (props) => {
                         <Typography color="textSecondary" gutterBottom>
                             Infected
                         </Typography>
-                        <Typography variant="h5">RealData </Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
                         <Typography color="textSecondary">REAL DATE</Typography>
                         <Typography variant="body2">
                             Number of Active Cases of COVID-19
